f2880d1edc15ea8e7090d3e87e59f0c4d63c592a
Eugene Maximenko <zhek4770@gmail.com>
2021-07-27 00:08:21 +0300

    finished the 3rd task :)

--- 3-task.js
+++ 3-task.js
@@ -2,36 +2,19 @@
 
 Каков самый большой делитель числа 600851475143, являющийся простым числом? */
 
-/* 
-
-
-1. Создать цикл for для итерации по частному числа, начиная с 2-х.
-2. Проверять каждое целое число на простоту.
-3. Если оно простое, добавить его в массив простых чисел. Если нет, то брейкнуть цикл
-
-39/2 - не целый результат, континью
-39/3 - результат 13 он целый, проверка. 13 простое число. Выводим 13 и прекращаем работу
-
-20/2 - результат 10, видим что он делится на что-то еще кроме 10, выходим из подцикла
-
- */
-
-const number = 39
+const number = 13
 
 for (let divideOn = 2; divideOn < number; divideOn++) {
     let divideResult = number / divideOn;
 
-    // Console the whole numbers
     if (number % divideOn === 0) {
-        console.log(`${number} / ${divideOn} = ${divideResult}`)
 
-        for (let integer2nd = 2; integer2nd < divideResult; integer2nd++) {
-            
-            if (Number.isInteger(divideResult / integer2nd)) {
-                console.log(`${divideResult} не является простым числом`)
+        for (let simpleNumberDivider = 2; simpleNumberDivider < divideResult; simpleNumberDivider++) {
+
+            if (divideResult % simpleNumberDivider === 0) {
                 break;
-            } else if (divideResult / integer2nd === divideResult - 1) {
-                console.log(`${divideResult} / ${integer2nd} = ${divideResult / integer2nd}`)
+            } else if (simpleNumberDivider === divideResult - 1) {
+                return console.log(`${divideResult} - наибольший простой делитель числа ${number}!`)
             }
 
         }
