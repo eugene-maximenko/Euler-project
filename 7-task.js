0a975b442951254cfedba1a3239f5bb0c875c260
Eugene Maximenko <zhek4770@gmail.com>
2021-07-31 01:47:37 +0300

    resolved 7th task

--- 7-task.js
+++ 7-task.js
@@ -1,3 +1,22 @@
 /* Выписав первые шесть простых чисел, получим 2, 3, 5, 7, 11 и 13. Очевидно, что 6-е простое число - 13.
 
 Какое число является 10001-м простым числом? */
+
+let counter = 6
+
+
+for (let number = 14; number > 0; number++) {
+
+    for (let integer = 2; integer <= number; integer++) {
+
+        if (number % integer === 0 && integer < number) {
+            break;
+        } else if (integer === number) {
+            counter++;
+
+            if (counter === 10001) {
+                return console.log(number);
+            }
+        }
+    }
+}
