8dbded2b0146002ed0cfb8ab013065118446f724
tiger3120 <zhek4770@gmail.com>
2021-08-01 22:47:40 +0300

    Done

--- 10-task.js
+++ 10-task.js
@@ -1,3 +1,19 @@
 /* Сумма простых чисел меньше 10 равна 2 + 3 + 5 + 7 = 17.
 
 Найдите сумму всех простых чисел меньше двух миллионов. */
+
+let sum = 2
+
+for (let number = 3; number < 2000000; number+=2) {
+
+    for (let integer = 2; integer < number; integer++) {
+
+        if (number % integer === 0) {
+            break
+        } else if (number % integer !== 0 && integer === number - 1) {
+            sum += number
+        }
+    }
+}
+
+console.log(sum) // 142913828922
